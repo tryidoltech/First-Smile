c2cea2294eadafebf5adabca48205cd5130026f9
dps-broken <107934635+dps-broken@users.noreply.github.com>
2024-08-01 17:19:29 +0530

    fresh code

--- assets/js/script.js
+++ assets/js/script.js
@@ -403,12 +403,12 @@ const indicators = [...document.querySelectorAll('.indicators button')];
 let currentTestimonial = 0; // Default 0
 
 indicators.forEach((item, i) => {
-    item.addEventListener('click', () => {
-        indicators[currentTestimonial].classList.remove('active');
-        wrapper.style.marginLeft = `-${100 * i}%`;
-        item.classList.add('active');
-        currentTestimonial = i;
-    })
+  item.addEventListener('click', () => {
+    indicators[currentTestimonial].classList.remove('active');
+    wrapper.style.marginLeft = `-${100 * i}%`;
+    item.classList.add('active');
+    currentTestimonial = i;
+  })
 })
 
 
